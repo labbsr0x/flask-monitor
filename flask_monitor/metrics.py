1078b3fa464040fc5d43b846d254f428f33eba65
Gustavo Coelho <gutorc@hotmail.com>
2020-01-30 14:30:01 -0300

    Big brother 0.2.0 metrics

--- flask_monitor/metrics.py
+++ flask_monitor/metrics.py
@@ -2,7 +2,7 @@ import time
 import threading
 import traceback
 import logging
-from flask import request, current_app
+from flask import request
 from prometheus_client import Counter, Histogram, Info, Gauge
 
 #
@@ -43,15 +43,18 @@ def register_metrics(app, buckets=[0.1, 0.3, 1.5, 10.5], error_fn=None):
     METRICS_REQUEST_LATENCY = Histogram(
         "request_seconds",
         "records in a histogram the number of http requests and their duration in seconds",
-        ["type", "status", "method", "addr", "version", "isError"],
+        ["type", "status", "isError", "method", "addr"],
         buckets=buckets
     )
 
     METRICS_REQUEST_SIZE = Counter(
         "response_size_bytes",
         "counts the size of each http response",
-        ["type", "status", "method", "addr", "version", "isError"],
+        ["type", "status", "isError", "method", "addr"],
     )
+
+    app_version = app.config.get("APP_VERSION", "0.0.0")
+    METRICS_INFO.labels(app_version).set(1)
     
     def before_request():
         """
@@ -64,15 +67,14 @@ def register_metrics(app, buckets=[0.1, 0.3, 1.5, 10.5], error_fn=None):
         """
         Register Prometheus metrics after each request
         """
-        app_version = current_app.config.get("APP_VERSION", "0.0.0")
         size_request = int(response.headers.get("Content-Length", 0))
         request_latency = time.time() - request._prometheus_metrics_request_start_time
         error_status = is_error(response.status_code)
         METRICS_REQUEST_LATENCY \
-            .labels("http", response.status_code, request.method, request.path, app_version, error_status) \
+            .labels("http", response.status_code, error_status, request.method, request.path) \
             .observe(request_latency)
         METRICS_REQUEST_SIZE.labels(
-            "http", response.status_code, request.method, request.path, app_version, error_status
+            "http", response.status_code, error_status, request.method, request.path
         ).inc(size_request)
         return response
     if error_fn is not None:

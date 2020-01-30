ae5b0851f546088e4d38905e7344e3e86feac19a
Gustavo Coelho <gutorc@hotmail.com>
2020-01-30 15:36:51 -0300

    Unique example of library use

--- example/simple_example.py
+++ example/simple_example.py
@@ -3,21 +3,31 @@ from prometheus_client import make_wsgi_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
 import traceback
-from flask_monitor import register_metrics
+from flask_monitor import register_metrics, watch_dependencies
 from flask import Flask
-import threading
+import requests as req
+
 app = Flask(__name__)
 app.config["APP_VERSION"] = "v0.1.2"
 
-register_metrics(app)
+def is_error200(code):
+    code = str(code) if type(code) is int else code
+    return code.startswith("2")
+
+register_metrics(app, buckets=[0.3, 0.6], error_fn=is_error200)
 # Plug metrics WSGI app to your main app with dispatcher
 dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
 
-def hello():
-    print("Teste")
+def check_db():
+    try:
+        response = req.get("http://localhost:5000/database")
+        if response.status_code == 200:
+            return 1
+    except:
+        traceback.print_stack()
+    return 0
 
-t = threading.Timer(3, hello)
-t.start()
+watch_dependencies("database", check_db, time_execution=10)
 
 @app.route('/teste')
 def hello_teste():
@@ -26,6 +36,10 @@ def hello_teste():
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/database')
+def bd_running():
+    return 'I am a database working.'
 
 if __name__ == "__main__":
     run_simple(hostname="localhost", port=5000, application=dispatcher)
